e569d81697cabc2322871999154ab0a0410b83f6
Sámal Rasmussen <samal84@gmail.com>
2025-09-05 14:01:38 +0100

    Add types

---
+++ types/DateRangePicker.d.ts
@@ -0,0 +1,17 @@
+export {
+  DateRangePicker,
+  DatepickerLocale,
+  CustomFormat,
+  DateFormat,
+  BeforeShowDayResult,
+  BeforeShowMonthResult,
+  BeforeShowYearResult,
+  BeforeShowDecadeResult,
+  DatepickerOptions,
+  DateRangePickerOptions,
+  DatepickerEventDetail,
+  DatepickerEvent,
+  SetDateOptions,
+  UpdateOptions,
+  ExitEditModeOptions
+} from './main';
